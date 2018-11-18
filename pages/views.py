ecbac2f0748bf3f228cd7d7831e16a5669f7fe02
NathanielYoung <young.nathaniel95@gmail.com>
2018-11-17 22:29:10 -0700

    file transfer

---
+++ pages/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+# creates a new page
+def home(request):
+    return render(request, "home.html", {})
+
+def about(request):
+    from pages.namer import namer
+    return render(request, "about.html", {"my_stuff": namer})
+
+def contact(request):
+    return render(request, "contact.html", {})
